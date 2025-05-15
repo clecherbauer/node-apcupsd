dcacb5aad40074cc4b1f82547ca158b7e210e341
Christian Lecherbauer <Christian.Lecherbauer@googlemail.com>
2025-05-15 18:53:57 +0200

    set pm2 cwd dynamically

--- apcupsd-mqtt.config.example.js
+++ apcupsd-mqtt.config.example.js
@@ -1,9 +1,15 @@
+const path = require('path');
+const baseDir = path.resolve(__dirname);
+
 module.exports = {
     apps: [{
         name: 'apcupsd-mqtt',
         script: './app.js',
+        cwd: baseDir,
         env: {
             MQTT_URL: 'mqtt://localhost',
         }
     }]
 };
+
+
